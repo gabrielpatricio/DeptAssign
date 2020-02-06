bbba4e4b3d284c192fd5205c993b8db00d77feb2
Gabriel Soares Patrício <gpatricio@ua.pt>
2020-02-06 18:20:20 +0000

    Implement autocomplete feature

--- DepTrailersApp/ClientApp/src/app/navbar/navbar.component.ts
+++ DepTrailersApp/ClientApp/src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import {MovieService} from '../services/movie.service';
 import { Movie } from '../models/movie';
 import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Router, ActivatedRoute } from '@angular/router';
 
 
@@ -14,10 +15,21 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   query = new FormControl('');
+  results$: Movie[] = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private movieService: MovieService) { }
 
   ngOnInit() {
+
+    this.query.valueChanges
+      .pipe(debounceTime(200),
+        distinctUntilChanged(),
+        switchMap((query) => this.movieService.getSearchResults(query)))
+      .subscribe(result => {
+        if (result === []) { return; }
+        else { this.results$ = result; }
+      });
+  
   }
   search() {
         this.router.navigate(['/search'], { queryParams: { q: this.query.value } 
